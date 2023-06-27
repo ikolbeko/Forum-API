e109dfc218528a6f5dad5a8428cdad8ab7ac2714
Ilya Kolbeko <ikolbeko@outlook.com>
2023-06-27 18:29:50 +0300

    add empty project

---
+++ Forum/Program.cs
@@ -0,0 +1,14 @@
+namespace Forum;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
